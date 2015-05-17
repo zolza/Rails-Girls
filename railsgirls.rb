ce7083ee7f151bb3f2d24bd212094af7d99f3260
zolza <kinga.koziol6@gmail.com>
2015-05-17 23:30:27 +0200

    Rails Girls tutorial on diary app

---
+++ railsgirls.rb
@@ -0,0 +1,20 @@
+a.each do |c|
+  puts c
+  a[5]
+end
+
+a.size.times do |i|
+  a[i]
+end
+
+10.times do |i|
+  puts "Hai " + i
+end
+
+(0...10).each do |i|
+end
+
+for i=0; i < a.size; i++ {
+  c = a[i]
+  puts c
+}
